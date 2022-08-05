053c9b0f9a81a820f6d45d506ba7f9c5e855d608
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2022-08-04 22:48:58 -0400

    v 0.03 - Winner Calculator
    - Determines who wins based off of the deaths
    - Calculates the obunga who got the most kills
    - made obunga spawning a percentage of the round timer (there will be 4 obungas in total)
    - other improvements

--- obungachase/gamemode/round_controller/sv_round_controller.lua
+++ obungachase/gamemode/round_controller/sv_round_controller.lua
@@ -1,5 +1,4 @@
 local round_status = 0 -- 0 = end, 1 = active
-local t = 1 -- time counter variable
 local round_length = 40-- Seconds
 
 util.AddNetworkString("UpdateRoundStatus")
@@ -19,16 +18,25 @@ function beginRound()
         
         -- Give Loadout to New Players
         local ply = player.GetAll()
-        print(ply)
+        print(ply[1]:Deaths())
         for k, v in pairs(ply) do
+            ply[k]:RemoveAllItems()
+
             ply[k]:Give("weapon_stunstick")
+            ply[k]:SetDeaths( 0 )
+            ply[k]:SetModel("models/player/gman_high.mdl")
         end
 
         -- Other Things Timer
+        local t = 1 -- time counter variable
         timer.Create("RoundTimer", 1, round_length, function()
 
             -- Creates and obunga at every specific time using player spawn locations
-            if (t == 1 or t == 20) then 
+            local ts1 = math.Round( 1*round_length/4) -- Spawn Times
+            local ts2 = math.Round( 2*round_length/4)
+            local ts3 = math.Round( 3*round_length/4)
+
+            if (t == 1 or t == ts1 or t == ts2 or t == ts3) then 
                 local tempObunga = ents.Create("npc_obunga")
                 local spawns = ents.FindByClass("info_player_start")
                 local random_spawn = math.random(#spawns)
@@ -41,9 +49,22 @@ function beginRound()
 
         -- Update Client Round Status
         updateClientRoundStatus()
+
+        -- Message:
+        PrintMessage(HUD_PRINTTALK, "[OC] Round Starting")
     end
 
 end
+
+-- Player Spawn Hook:
+local function Respawn( ply )
+    if ply:Deaths() > 0 then
+        ply:Give("weapon_crowbar")
+        ply:SetModel("models/kaydax/gman_obunga.mdl")
+	    print( "[OC] ".. ply:Nick() .. " has respawned as an Obunga!" )
+    end
+end
+hook.Add( "PlayerSpawn", "Respawn", Respawn)
 
 -- End Round:
 function endRound()
@@ -60,6 +81,34 @@ function endRound()
 
     -- Update Client of the Round Status
     updateClientRoundStatus()
+    
+    -- Winner Messages:
+    local ply = player.GetAll()
+    for k, v in pairs(ply) do
+
+        if ply[k]:Deaths() == 0 then
+            PrintMessage(HUD_PRINTTALK, "[OC] "..ply[k]:Nick().." is a winner!")
+        end
+    end
+    
+    -- Obunga with most kills:
+    local maxkills = 0
+    for k, v in pairs(ply) do
+        if ply[k]:Deaths() > 0 and ply[k]:Frags() > maxkills then
+            maxkills = ply[k]:Frags()
+        end
+    end
+
+    if maxkills > 0 then
+        for k, v in pairs(ply) do
+            if maxkills == ply[k]:Frags() then
+                PrintMessage(HUD_PRINTTALK, "[OC]"..ply[k]:Nick().." had the most kills (".. maxkills .. ") as an obunga!")
+            end
+        end
+    end
+
+    -- Message:
+    PrintMessage(HUD_PRINTTALK, "[OC] Round Over")
 
 end
 
