b6071b85c5fc95283fff448c4caae716a1b64613
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2022-08-14 20:43:28 -0400

    v0.11.03 - Multiple Nextbots
    - Added support for the rock
    - Added support for one custom next bot
    - Changed the way that nextbots are spawned and deleted to allocate some of that functionality to sv_settings

--- obungachase/gamemodes/obungachase/gamemode/settings/sv_settings.lua
+++ obungachase/gamemodes/obungachase/gamemode/settings/sv_settings.lua
@@ -2,12 +2,41 @@
 
 -- Choose Nextbots:
 
-local Nextbots = {"npc_obunga"}
+local Nextbots = {"npc_obunga","npc_therock"}
+local NextbotsOn = {GetConVar("q_npc_obunga"):GetBool(),GetConVar("q_npc_therock"):GetBool()}
+local customBot = GetConVar("q_npc_custom"):GetString()
+if customBot == GetConVar("q_npc_custom"):GetDefault() then
+    customBot = 0
+end
 
-function selectBot()
+function getActiveNextbots()
+    -- Load Counter Variable:
+    local c = 1
+    local sNextbots = {}
 
-    sNextbots = Nextbots[1]
+    -- Apply Custom bot:
+    if customBot ~= 0 then
+        sNextbots[c] = customBot
+        c = c + 1
+    end
+
+    -- Find Hard Programed Bots that are On
+    for k, v in pairs(Nextbots) do
+        if NextbotsOn[k] then
+            sNextbots[c] = v
+            c = c + 1
+        end
+    end
 
     return sNextbots
+end
+
+function selectBot()
+    
+    sNextbots = getActiveNextbots()
+
+    -- Decide Random Bot
+    selectedBot = sNextbots[math.random(1, #sNextbots)]
+    return selectedBot
     
 end
--- obungachase/gamemodes/obungachase/gamemode/shared.lua
+++ obungachase/gamemodes/obungachase/gamemode/shared.lua
@@ -1,4 +1,4 @@
--- Obunga Chase v0.11.02
+-- Obunga Chase v0.11.03
 GM.Name = "Obunga Chase"
 GM.Author = "MajorRelativity"
 GM.Website = "https://github.com/MajorRelativity/Obunga-Chase"
